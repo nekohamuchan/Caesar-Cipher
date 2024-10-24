77fbd89c2b24b65c3b792b0992a7e22f0a60d174
Hsin Yi Huang <151099887+nekohamuchan@users.noreply.github.com>
2024-10-24 09:59:11 +0800

    Edit and clean codes

--- script.rb
+++ script.rb
@@ -1,35 +1,11 @@
 def caesar_cipher(str, shift_factor)
-  ascii = str.chars.map do |e|
-    if e.match?(/[[:alpha:]]/)
-      e.ord
-    else
-      e
-    end
-  end
-  
-  shifted = ascii.map do |e|
-    if e.is_a? Numeric
-      if (e + shift_factor > 90 && e + shift_factor < 97) || (e + shift_factor > 122)
-        e + shift_factor - 26
-      else
-        e + shift_factor
-      end
-    else
-      e
-    end
-  end
-
-  result = shifted.map do |e|
-    if e.is_a? Numeric
-      e.chr
-    else
-      e
-    end
-  end
+  ascii = str.chars.map { |e| (e.ord if e.match?(/[[:alpha:]]/)) || e }
+  shifted = ascii.map { |e| (e + shift_factor if e.is_a? Numeric) || e }
+  wraped = shifted.map { |e| (e - 26 if (e.is_a? Numeric) && ((e > 90 && e < 97) || (e > 122))) || e }
+  result = wraped.map { |e| (e.chr if e.is_a? Numeric) || e }
 
   result.join("")
 end
-
 
 puts "Caesar Cipher"
 puts "Please enter your text:"
